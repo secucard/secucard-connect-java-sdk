ba482707eedb3c53f7a5ea276c98e860bc44ac2d
tkrau55 <thomas.krauss@posteo.de>
2014-11-07 17:55:57 +0100

    api style examples added

--- src/main/java/com/secucard/connect/example/service/GeneralService.java
+++ src/main/java/com/secucard/connect/example/service/GeneralService.java
@@ -2,7 +2,6 @@ package com.secucard.connect.example.service;
 
 import com.secucard.connect.BaseClient;
 import com.secucard.connect.ClientConfig;
-import com.secucard.connect.jackson.general.merchant.Merchant;
 import com.secucard.connect.model.general.skeleton.Skeleton;
 
 import java.util.List;
@@ -12,9 +11,7 @@ public class GeneralService extends BaseClient {
   public Skeleton getSkeleton(String id){
     return selectChannnel().getObject(Skeleton.class, id);
   }
-  public Merchant getMerchant(String id){
-    return selectChannnel().getObject(Merchant.class, id);
-  }
+
 
   public static GeneralService create(ClientConfig config) {
     return BaseClient.create(config, GeneralService.class);

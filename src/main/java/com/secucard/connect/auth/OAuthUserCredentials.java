e5cfb7db27db0de5ccd91e6a9142d583ede482f3
Steffen Schroeder <schroeder@devid.net>
2014-11-27 15:10:22 +0100

    Update build.gradle Format OAuthUserCredentials.

--- src/main/java/com/secucard/connect/auth/OAuthUserCredentials.java
+++ src/main/java/com/secucard/connect/auth/OAuthUserCredentials.java
@@ -3,36 +3,33 @@
  */
 package com.secucard.connect.auth;
 
-import com.secucard.connect.auth.OAuthUserCredentials;
-
-import javax.ws.rs.core.MultivaluedMap;
-
 /**
  * Password credentials used for authorization
  */
 public class OAuthUserCredentials {
-	private final String username;
-	private final String password;
+    private final String username;
+    private final String password;
 
-	/**
-	 * Constructor
-	 * @param username
-	 * @param password
-	 */
-	public OAuthUserCredentials(String username, String password) {
-		super();
-		this.username = username;
-		this.password = password;
-	}
+    /**
+     * Constructor
+     *
+     * @param username
+     * @param password
+     */
+    public OAuthUserCredentials(String username, String password) {
+        super();
+        this.username = username;
+        this.password = password;
+    }
 
-	/**
-	 * Getters
-	 */
-	public String getUsername() {
-		return username;
-	}
+    /**
+     * Getters
+     */
+    public String getUsername() {
+        return username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 }

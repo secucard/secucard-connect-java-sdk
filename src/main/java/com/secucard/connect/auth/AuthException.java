cac54e8bdeed2c041febb8a1bfa18d4ac32bd23c
tkrau55 <thomas.krauss@posteo.de>
2015-01-13 17:59:44 +0100

    Auth exception created

---
+++ src/main/java/com/secucard/connect/auth/AuthException.java
@@ -0,0 +1,19 @@
+package com.secucard.connect.auth;
+
+public class AuthException extends RuntimeException {
+  public AuthException() {
+    super();
+  }
+
+  public AuthException(String message) {
+    super(message);
+  }
+
+  public AuthException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public AuthException(Throwable cause) {
+    super(cause);
+  }
+}

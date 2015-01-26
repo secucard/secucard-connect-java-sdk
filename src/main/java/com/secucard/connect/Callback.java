f5ff9e7aedcd8bb53959478b22669e552a70d360
tkrau55 <thomas.krauss@posteo.de>
2015-01-26 11:27:22 +0100

    javadoc added

--- src/main/java/com/secucard/connect/Callback.java
+++ src/main/java/com/secucard/connect/Callback.java
@@ -1,8 +1,24 @@
 package com.secucard.connect;
 
+/**
+ * Implementing  instances can be passed to methods to get asyncronous notified when the
+ * operation performed by the method completes or failes.
+ *
+ * @param <T> The type of the operations result.
+ */
 public interface Callback<T> {
 
+  /**
+   * Gets called when the operation is sucessfully completed.
+   *
+   * @param result The value returned by the operation.
+   */
   void completed(T result);
 
-  void failed(Throwable throwable);
+  /**
+   * Gets called when the operation has failed.
+   *
+   * @param cause The fail cause.
+   */
+  void failed(Throwable cause);
 }

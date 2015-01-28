80adbb0ca448198cff8deb3b3384445a4e8c7767
Steffen Schroeder <schroeder@devid.net>
2015-01-28 14:47:10 +0100

    Change Callback import.

--- src/main/java/com/secucard/connect/service/general/AccountService.java
+++ src/main/java/com/secucard/connect/service/general/AccountService.java
@@ -1,5 +1,6 @@
 package com.secucard.connect.service.general;
 
+import com.secucard.connect.Callback;
 import com.secucard.connect.model.general.accounts.Account;
 import com.secucard.connect.model.general.accounts.Location.Location;
 import com.secucard.connect.model.transport.Result;

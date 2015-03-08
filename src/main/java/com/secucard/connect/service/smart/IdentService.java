8c503b41fd6a0870e3cc870da6c7a7485655205b
tkrau55 <thomas.krauss@posteo.de>
2015-03-08 20:32:42 +0100

    smart services via rest (scjava-29)

--- src/main/java/com/secucard/connect/service/smart/IdentService.java
+++ src/main/java/com/secucard/connect/service/smart/IdentService.java
@@ -1,6 +1,7 @@
 package com.secucard.connect.service.smart;
 
 import com.secucard.connect.Callback;
+import com.secucard.connect.ClientContext;
 import com.secucard.connect.model.ObjectList;
 import com.secucard.connect.model.smart.Ident;
 import com.secucard.connect.service.AbstractService;
@@ -12,17 +13,11 @@ public class IdentService extends AbstractService {
   /**
    * Returns all idents in the system or null if nothing found.
    */
-  public List<Ident> getIdents(Callback<List<Ident>> callback) {
-    return new ConvertingInvoker<ObjectList<Ident>, List<Ident>>() {
-      @Override
-      protected List<Ident> convert(ObjectList<Ident> value) {
-        return value == null ? null : value.getList();
-      }
+  public ObjectList<Ident> getIdentsList(Callback<ObjectList<Ident>> callback) {
+    return getObjectList(Ident.class, null, callback, ClientContext.STOMP);
+  }
 
-      @Override
-      protected ObjectList<Ident> handle(Callback<ObjectList<Ident>> callback) throws Exception {
-        return getChannel().findObjects(Ident.class, null, callback);
-      }
-    }.invokeAndConvert(callback);
+  public List<Ident> getIdents(Callback<List<Ident>> callback) {
+    return getList(Ident.class, null, callback, ClientContext.STOMP);
   }
 }

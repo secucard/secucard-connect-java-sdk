3e6c6ec8b5d850037c9675edc440abc3466b0fdc
André Mommert <a.mommert@secupay.ag>
2017-07-13 15:05:26 +0200

    no message

--- src/main/java/com/secucard/connect/SecucardConnect.java
+++ src/main/java/com/secucard/connect/SecucardConnect.java
@@ -57,7 +57,7 @@ import java.util.TimerTask;
  * The entry point to the secucard API, provides resources for product operations.
  */
 public class SecucardConnect {
-  public static final String VERSION = "2.3.0";
+  public static final String VERSION = "2.4.0";
 
   protected volatile boolean isConnected;
   private Configuration configuration;

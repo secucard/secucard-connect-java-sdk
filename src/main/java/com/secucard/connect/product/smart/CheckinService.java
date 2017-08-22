2d83092cfa19743fe848d28564a8de6ff07928d8
Rico Simlinger <r.simlinger@secupay.ag>
2017-08-22 15:39:07 +0200

    Update CheckinService.java

--- src/main/java/com/secucard/connect/product/smart/CheckinService.java
+++ src/main/java/com/secucard/connect/product/smart/CheckinService.java
@@ -1,15 +1,3 @@
-/*
- * Copyright (c) 2015. hp.weber GmbH & Co secucard KG (www.secucard.com)
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.secucard.connect.product.smart;
 
 import com.secucard.connect.client.Callback;
@@ -40,6 +28,24 @@ public class CheckinService extends ProductService<Checkin> {
   @Override
   public Options getDefaultOptions() {
     return new Options(Options.CHANNEL_STOMP);
+  }
+
+  /**
+   * Set a callback to get notified when a check in happened.
+   */
+  public void onCheckinSelected(final Callback<Checkin> callback) {
+    AbstractEventListener listener = null;
+
+    if (callback != null) {
+      listener = new DelegatingEventHandlerCallback<Event, Checkin>(callback) {
+        @Override
+        protected Checkin process(Event event) {
+          return get(); // TODO: parameter prüfen
+        }
+      };
+    }
+
+    context.eventDispatcher.registerListener(getMetaData().getObject() + Events.TYPE_CHANGED, listener);
   }
 
   /**
